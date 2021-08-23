4490ffedc4c31220c74e019a5eeb77065aff5511
jorgestilli <89410144+jorgestilli@users.noreply.github.com>
2021-08-23 14:30:31 -0300

    Add files via upload

---
+++ desafio3.js
@@ -0,0 +1,7 @@
+//Este Desafio 3 es una calculadora de multiplicaion del 0 al 10
+
+let numero = prompt("Ingrese un numero")
+for (let i = 0; i<=10; i++) {
+    resultado= numero*i
+alert(numero+ "X" +i+ "="+resultado)
+}
